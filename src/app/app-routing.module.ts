cdb1e38dbc982d93f954bf8273b908a06608391a
nparsley <nickparsley5@gmail.com>
2021-04-09 16:34:16 -0700

    cart,food component add + menuitem,fooddata add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { FoodComponent } from './food/food.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cart', component: CartComponent},
+  { path: '', component: FoodComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
